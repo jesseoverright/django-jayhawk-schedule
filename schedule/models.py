41bd992c254d06509f6c405ff36643cb7d486af2
Jesse Overright <jesseoverright@gmail.com>
2013-11-08 10:07:22 -0600

    updated how title is generated

--- schedule/models.py
+++ schedule/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+GAME_TYPES = (
+    ('Exhibition', 'Exhibition'),
+    ('Non Conference', 'Non-Conference'),
+    ('Preseason Tournament', 'Preseason Tournament'),
+    ('Conference', 'Conference'),
+    ('Conference Tournament', 'Conference Tournament'),
+    ('NCAA Tournament', 'NCAA Tournament'),
+)
+
 # a Game on KU's schedule includes opponent info, date, location and tv details
 class Game(models.Model):
     opponent = models.CharField(max_length=255)
@@ -9,14 +18,7 @@ class Game(models.Model):
     location = models.CharField(max_length=255)
     television = models.CharField(max_length=255)
     date = models.DateTimeField()
-    game_type = models.CharField(max_length=25, choices=[
-        ('Exhibition', 'Exhibition'),
-        ('Non Conference', 'Non-Conference'),
-        ('Preseason Tournament', 'Preseason Tournament'),
-        ('Conference', 'Conference'),
-        ('Conference Tournament', 'Conference Tournament'),
-        ('NCAA Tournament', 'NCAA Tournament'),
-        ])
+    game_type = models.CharField(max_length=25, choices=GAME_TYPES)
     score = models.IntegerField(null=True, blank=True)
     opponent_score = models.IntegerField(null=True, blank=True)
 
@@ -32,7 +34,7 @@ class Game(models.Model):
         ordering = ['-date']
 
     def __unicode__(self):
-        return u'%s' % self.opponent
+        return u'%s %s' % (self.opponent, self.date.strftime('%b %d'))
 
     def get_absolute_url(self):
         return reverse('schedule.views.game', args=[self.slug])
--- schedule/views.py
+++ schedule/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from schedule.models import Game
 
 def index(request):
-    # get games
     games = Game.objects.order_by('date')
 
     return render(request, 'schedule/index.html', {'games': games})
@@ -11,4 +10,7 @@ def index(request):
 def game(request, slug):
     game = get_object_or_404(Game, slug=slug)
 
-    return render(request, 'schedule/game.html', {'game': game})
+    return render(request, 'schedule/game.html', {
+        'title': 'KU vs %s' % game.opponent,
+        'game': game
+    })
