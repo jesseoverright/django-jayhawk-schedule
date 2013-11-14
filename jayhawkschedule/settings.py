aa2512b16c66e669f58113da8cab0c6ee1e0fc39
Jesse Overright <jesseoverright@gmail.com>
2013-11-14 15:30:49 -0600

    fixed debug settings & allowed hosts

--- jayhawkschedule/settings.py
+++ jayhawkschedule/settings.py
@@ -24,7 +24,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
         'LOCATION': 'api_cache',
-        'TIMEOUT': 60*10, # 10 minutes
+        'TIMEOUT': 60*60, # 1 hour
         'VERSION': 1,
         'OPTIONS': {
             'MAX_ENTRIES': 10000
@@ -34,7 +34,7 @@ CACHES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.jayhawkschedule.co']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
