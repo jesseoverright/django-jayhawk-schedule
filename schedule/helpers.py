c8993809537580a23e2af673e29e04c692f247b8
Jesse Overright <jesseoverright@gmail.com>
2014-01-30 11:56:58 -0600

    Fixed a bug with dedeuped lists not working

--- schedule/helpers.py
+++ schedule/helpers.py
@@ -1,9 +1,11 @@
 def dedupe_lists(list1, list2, count=100):
-    ordered_list = sorted(list1 + list2, key=lambda k: k['published'], reverse=True)
+    ordered_list = sorted(list1 + list2, key=lambda k: k['id'], reverse=True)
     deduped_list = []
-    deduped_items = 0
-    for item in (ordered_list):
-        if item not in deduped_list and deduped_items < count:
+    deduped_items = []
+    deduped_count = 0
+    for item in ordered_list:
+        if item['id'] not in deduped_items and deduped_count < count:
             deduped_list.append(item)
-            deduped_items += 1
+            deduped_items.append(item['id'])
+            deduped_count += 1
     return deduped_list
