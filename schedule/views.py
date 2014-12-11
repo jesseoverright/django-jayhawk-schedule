5954f4b7553aa49d03f78fc3ab5d8fd4c7e2df2f
Jesse Overright <jesseoverright@gmail.com>
2014-12-10 20:27:35 -0600

    rollback update

--- schedule/views.py
+++ schedule/views.py
@@ -60,7 +60,6 @@ def game(request, season, slug):
     game = get_object_or_404(Game, season=season, slug=slug)
 
     if game.get_result() == 'win' or game.get_result() == 'loss':
-        game.get_game_recaps(4)
         game.get_news(4)
     else:
         game.get_news(8)
